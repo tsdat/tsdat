c36d05958369fed59b6b23041c9d1ed930dc2600
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-06 14:52:56 -0800

    Implemented FileSystemStorage (untested)

--- tsdat/io/storage.py
+++ tsdat/io/storage.py
@@ -1,8 +1,10 @@
 import os
 import abc
+import shutil
 import zipfile
 import datetime
 from typing import List
+from tsdat.standards import Standards
 
 class DatastreamStorage(abc.ABC):
 
@@ -94,3 +96,141 @@ class FilesystemStorage(DatastreamStorage):
 
     def __init__(self, root: str):
       self.__root = root
+
+    @staticmethod
+    def get_date_from_filename(filename: str) -> str:
+        """-------------------------------------------------------------------
+        Given a filename that conforms to MHKiT-Cloud Data Standards, return 
+        the date of the first point of data in the file. 
+
+        Args:
+            filename (str): The filename or path to the file.
+
+        Returns:
+            str: The date, in "yyyymmdd.hhmmss" format.
+        -------------------------------------------------------------------"""
+        filename = os.path.basename(filename)
+        date = filename.split(".")[1]
+        time = filename.split(".")[2]
+        return f"{date}.{time}"
+
+    def fetch(self, datastream_name: str, start_time: str, end_time: str, local_path: str) -> List[str]:
+        """-------------------------------------------------------------------
+        Fetches a file from the filesystem store using the datastream_name, 
+        start_time, and end_time to specify the file(s) to retrieve. The 
+        retrieved files will be saved in the directory `local_path`.
+
+        Args:
+            datastream_name (str):  The datastream_name as defined by 
+                                    MHKiT-Cloud Data Standards.
+            start_time (str):   The start time or date to start searching for
+                                data (inclusive). Should be like "20210106" to
+                                search for data beginning on or after 
+                                January 6th, 2021.
+            end_time (str): The end time or date to stop searching for data
+                            (exclusive). Should be like "20210108" to search
+                            for data ending before January 8th, 2021.
+            local_path (str):   The path to the directory where the data 
+                                should be stored. 
+
+        Returns:
+            List[str]:  A list of paths where the retrieved files were stored
+                        in local storage.  
+        -------------------------------------------------------------------"""
+        store_dir = Standards.get_datastream_path(datastream_name, self.__root)
+        files = [f for f in os.listdir(store_dir) if start_time <= self.get_date_from_filename(f) < end_time]
+        sources = [os.path.join(store_dir, file) for file in files]
+        targets = [os.path.join(local_path, file) for file in files]
+        for source, target in zip(sources, targets):
+            shutil.copy(source, target)
+        return targets
+    
+    def save(self, local_path: str) -> None:
+        """-------------------------------------------------------------------
+        Saves a local file to the appropriate location in the filesystem.
+
+        Args:
+            local_path (str):   The path to the local file to save. The file 
+                                should be named according to MHKiT-Cloud 
+                                naming conventions.
+        -------------------------------------------------------------------"""
+        # TODO: copy to storage
+        filename = os.path.basename(local_path)
+        Standards.validate_filename(filename)
+        target_dir = Standards.get_datastream_path(filename=filename, root=self.__root)
+        shutil.copy(local_path, os.path.join(target_dir, filename))
+        return
+    
+    def exists(self, datastream_name: str, start_time: str, end_time: str) -> bool:
+        """-------------------------------------------------------------------
+        Checks if data exists in the filesystem for the provided datastream 
+        and time range.
+
+        Args:
+            datastream_name (str):  The datastream_name as defined by 
+                                    MHKiT-Cloud Data Standards.
+            start_time (str):   The start time or date to start searching for
+                                data (inclusive). Should be like "20210106" to
+                                search for data beginning on or after 
+                                January 6th, 2021.
+            end_time (str): The end time or date to stop searching for data
+                            (exclusive). Should be like "20210108" to search
+                            for data ending before January 8th, 2021.
+
+        Returns:
+            bool: True if data exists, False otherwise.
+        -------------------------------------------------------------------"""
+        dir_to_check = Standards.get_datastream_path(datastream_name=datastream_name, root=self.__root)
+        for file in os.listdir(dir_to_check):
+            if start_time <= self.get_date_from_filename(file) < end_time:
+                return True
+        return False
+    
+    def _find(self, datastream_name: str, start_time: str, end_time: str) -> List[str]:
+        """-------------------------------------------------------------------
+        Returns paths to the datastream's files in the filesystem where the 
+        start and end times of the data files fall within the provided 
+        start_time and end_time range.
+
+        Args:
+            datastream_name (str):  The datastream_name as defined by 
+                                    MHKiT-Cloud Data Standards.
+            start_time (str):   The start time or date to start searching for
+                                data (inclusive). Should be like "20210106" to
+                                search for data beginning on or after 
+                                January 6th, 2021.
+            end_time (str): The end time or date to stop searching for data
+                            (exclusive). Should be like "20210108" to search
+                            for data ending before January 8th, 2021.
+
+        Returns:
+            bool: True if data exists, False otherwise.
+        -------------------------------------------------------------------"""
+        dir_to_check = Standards.get_datastream_path(datastream_name=datastream_name, root=self.__root)
+        storage_paths = []
+        for file in os.listdir(dir_to_check):
+            if start_time <= self.get_date_from_filename(file) < end_time:
+                storage_paths.append(os.path.join(dir_to_check, file))
+        return storage_paths
+
+    def delete(self, datastream_name: str, start_time: str, end_time: str) -> None:
+        """-------------------------------------------------------------------
+        Deletes datastream data in the datastream store in between the 
+        specified time range. 
+
+        Args:
+            datastream_name (str):  The datastream_name as defined by 
+                                    MHKiT-Cloud Data Standards.
+            start_time (str):   The start time or date to start searching for
+                                data (inclusive). Should be like "20210106" to
+                                search for data beginning on or after 
+                                January 6th, 2021.
+            end_time (str): The end time or date to stop searching for data
+                            (exclusive). Should be like "20210108" to search
+                            for data ending before January 8th, 2021.
+        -------------------------------------------------------------------"""
+        files_to_delete = self._find(datastream_name, start_time, end_time)
+        for file in files_to_delete:
+            os.remove(file)
+        return
+
