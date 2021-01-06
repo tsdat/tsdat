7481412c4b79699b5be3a1b7ebddc24cd85a34f4
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-06 11:39:37 -0800

    Updated base class to use datastream_name, start_time, and end_time instead of datastream_store_path. Removed listdir() and rename()

--- tsdat/io/storage.py
+++ tsdat/io/storage.py
@@ -1,90 +1,88 @@
 import os
 import abc
 import zipfile
-from typing import List, Tuple
+import datetime
+from typing import List
 
 class DatastreamStorage(abc.ABC):
 
     @abc.abstractmethod
-    def fetch(self, datastream_store_path: str, local_path: str) -> str:
+    def fetch(self, datastream_name: str, start_time: str, end_time: str, local_path: str) -> List[str]:
         """-------------------------------------------------------------------
-        Fetches a file from the datastream store. If the local path is not 
-        specified, it is up to the subclass to determine where to put the 
-        retrieved file.
+        Fetches a file from the datastream store using the datastream_name, 
+        start_time, and end_time to specify the file(s) to retrieve. If the 
+        local path is not specified, it is up to the subclass to determine
+        where to put the retrieved file(s).
 
         Args:
-            datastream_store_path (str):    The path to the file in the 
-                                            datastream storage to fetch.
-            local_path (str):   The path to where the retrieved file should 
-                                be saved.
+            datastream_name (str):  The datastream_name as defined by 
+                                    MHKiT-Cloud Data Standards.
+            start_time (str):   The start time or date to start searching for
+                                data (inclusive). Should be like "20210106" to
+                                search for data beginning on or after 
+                                January 6th, 2021.
+            end_time (str): The end time or date to stop searching for data
+                            (exclusive). Should be like "20210108" to search
+                            for data ending before January 8th, 2021.
+            local_path (str):   The path to the directory where the data 
+                                should be stored. 
 
         Returns:
-            str: Returns the path to the retrieved file in local storage.
+            List[str]:  A list of paths where the retrieved files were stored
+                        in local storage.  
         -------------------------------------------------------------------"""
         return
     
     @abc.abstractmethod
-    def save(self, local_path: str, datastream_store_path: str) -> None:
+    def save(self, local_path: str) -> None:
         """-------------------------------------------------------------------
         Saves a local file to the datastream store.
 
         Args:
-            local_path (str): The path to the local file to save.
-            datastream_store_path (str):    The path in the datastream store 
-                                            where the file should be saved to.
+            local_path (str):   The path to the local file to save. The file 
+                                should be named according to MHKiT-Cloud 
+                                naming conventions.
         -------------------------------------------------------------------"""
         return
 
     @abc.abstractmethod
-    def exists(self, datastream_store_path: str) -> bool:
+    def exists(self, datastream_name: str, start_time: str, end_time: str) -> bool:
         """-------------------------------------------------------------------
-        Checks if the file exists in the datastream store
+        Checks if data exists in the datastream store for the provided 
+        datastream and time range.
 
         Args:
-            datastream_store_path (str): The path to the file to check.
+            datastream_name (str):  The datastream_name as defined by 
+                                    MHKiT-Cloud Data Standards.
+            start_time (str):   The start time or date to start searching for
+                                data (inclusive). Should be like "20210106" to
+                                search for data beginning on or after 
+                                January 6th, 2021.
+            end_time (str): The end time or date to stop searching for data
+                            (exclusive). Should be like "20210108" to search
+                            for data ending before January 8th, 2021.
 
         Returns:
-            bool: True if the file exists, False otherwise.
+            bool: True if data exists, False otherwise.
         -------------------------------------------------------------------"""
         return
 
     @abc.abstractmethod
-    def delete(self, datastream_store_path: str) -> None:
+    def delete(self, datastream_name: str, start_time: str, end_time: str) -> None:
         """-------------------------------------------------------------------
-        Deletes a file in the datastream store
+        Deletes datastream data in the datastream store in between the 
+        specified time range. 
 
         Args:
-            datastream_store_path (str): The path to the file to delete.
-        -------------------------------------------------------------------"""
-        return
-
-    @abc.abstractmethod
-    def listdir(self, datastream_store_path: str) -> Tuple[List[str], List[str]]: # might not be needed
-        """-------------------------------------------------------------------
-        Lists the files and directories in the specified path and returns a 
-        2-tuple of lists where the first item is directories and the second 
-        item is files. 
-
-        Args:
-            datastream_store_path (str):    The directory whose contents 
-                                            should be listed.
-
-        Returns:
-            Tuple[List[str], List[str]]:    A 2-tuple with directories as the 
-                                            first item and files paths as the
-                                            second item.
-        -------------------------------------------------------------------"""
-        return
-    
-    @abc.abstractmethod
-    def rename(self, previous_datastream_store_path: str, new_datastream_store_path: str) -> None:
-        """-------------------------------------------------------------------
-        Renames a file in the datastream store.
-
-        Args:
-            previous_datastream_store_path (str):   The path to an existing 
-                                                    file to be renamed.
-            new_datastream_store_path (str): The desired new path to the file. 
+            datastream_name (str):  The datastream_name as defined by 
+                                    MHKiT-Cloud Data Standards.
+            start_time (str):   The start time or date to start searching for
+                                data (inclusive). Should be like "20210106" to
+                                search for data beginning on or after 
+                                January 6th, 2021.
+            end_time (str): The end time or date to stop searching for data
+                            (exclusive). Should be like "20210108" to search
+                            for data ending before January 8th, 2021.
         -------------------------------------------------------------------"""
         return
 
@@ -94,6 +92,5 @@ class FilesystemStorage(DatastreamStorage):
     DatastreamStorage subclass for a typical Linux-based filesystem.
     -----------------------------------------------------------------------"""
 
-    def __init__(self, root):
-      self.__root = root     
-
+    def __init__(self, root: str):
+      self.__root = root
