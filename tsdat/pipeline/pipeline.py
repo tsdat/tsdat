533468f54c93e1413c012701909882a09fc8bac7
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-11 13:54:48 -0800

    Added standardize and validate methods

--- tsdat/pipeline/pipeline.py
+++ tsdat/pipeline/pipeline.py
@@ -1,6 +1,8 @@
 import os
 import abc
+import xarray as xr
 from tsdat.config import Config
+from tsdat.standards import Standards
 from tsdat.io.storage import DatastreamStorage
 from tsdat.io.file_handlers import FILEHANDLERS, FileHandler
 
@@ -35,4 +37,43 @@ class Pipeline(abc.ABC):
         if ext not in FILEHANDLERS:
             raise KeyError(f"no FileHandler for extension: {ext}")
         return FILEHANDLERS[ext]
+
+    def standardize(self, dataset: xr.Dataset) -> xr.Dataset:
+        """-------------------------------------------------------------------
+        Uses the config object to map the input dataset variable names to the 
+        output dataset. Additionally, this function ensures that the dataset 
+        conforms to MHKiT-Cloud data standards. 
+
+        Args:
+            dataset (xr.Dataset):   The raw data into an xarray dataset.
+            config (Config): The config object associated with the dataset.
+
+        Returns:
+            xr.Dataset: The standardized dataset.
+        -------------------------------------------------------------------"""
+        definition = self.config.dataset_definition.to_dict()
+        
+        # definition["attributes"]
+        # definition["dimensions"]
+
+        # for variable in definition["variables"]:
+            # get original values
+            # convert to formatted values
+            # add the values back to the dict
+        
+        dataset = xr.Dataset(definition)
+        Standards.validate(dataset)
+        return dataset
+    
+    def validate_dataset(self, dataset: xr.Dataset):
+        """-------------------------------------------------------------------
+        Confirms that the dataset conforms with MHKiT-Cloud data standards. 
+        Raises an error if the dataset is improperly formatted. This method 
+        should be overridden if different standards or validation checks 
+        should be applied.
+
+        Args:
+            dataset (xr.Dataset): The dataset to validate.
+        -------------------------------------------------------------------"""
+        Standards.validate_dataset(dataset)
  
