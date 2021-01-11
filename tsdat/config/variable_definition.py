a6ee6d6b7c0f6e1b0a947fec2c1a79d3461efc9c
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-11 13:54:18 -0800

    Added variable type parsing

--- tsdat/config/variable_definition.py
+++ tsdat/config/variable_definition.py
@@ -1,8 +1,16 @@
 import numpy as np
-from typing import Dict
+from typing import Dict, List
+from .attribute_defintion import AttributeDefinition
+from .dimension_definition import DimensionDefinition
+
+class VarKeys:
+    INPUT = 'input'
+    DIMS = 'dims'
+    TYPE = 'type'
+    ATTRS = 'attrs'
 
 class VariableDefinition:
-    def __init__(self, name: str, dict: Dict[str, object]):
+    def __init__(self, name: str, dictionary: Dict):
         self.name = name
         self.input = dict.get("input", {})
         self.attrs = dict.get('attrs', {})
@@ -11,7 +19,7 @@ class VariableDefinition:
         for key in dict:
             if not hasattr(self, key):
                 setattr(self, key, dict[key])
-
+    
     def _parse_data_type(self, data_type: str):
         """-------------------------------------------------------------------
         Parses the data_type string and returns the appropriate numpy data 
@@ -27,15 +35,25 @@ class VariableDefinition:
                     types.
         -------------------------------------------------------------------"""
         mappings = {
-            "string":   "s256",     # dtype='s256' sets maximum length to 256
-            "char":     str,        # dtype=str sets maximum length to 1
+            "string":   np.str,
+            "char":     np.str,
             "byte":     np.int8,
+            "ubyte":    np.uint8,
             "short":    np.int16,
+            "ushort":   np.uint16,
+            "long":     np.int64,
+            "ulong":    np.uint64,
             "int":      np.int32,
             "float":    np.float32,
             "double":   np.float64
         }
-        return mappings.get(data_type, data_type)
+        if data_type not in mappings:
+            error_message = f"{data_type} is not a standard data type. Data type must be one of: \n{'\n'.join(list(mappings.keys()))}"
+            raise KeyError(error_message)
+        return mappings[data_type]
+
+    def _parse_dimensions(self, dictionary: Dict) -> List[str]:
+        return dictionary.get("dims", [])
 
     def is_constant(self) -> bool:
         """-------------------------------------------------------------------
