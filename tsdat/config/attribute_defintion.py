84236ae2e2e171b05c606146ae1570ba69a7ebda
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-11 21:58:32 -0800

    Attributes with None value are now ""

--- tsdat/config/attribute_defintion.py
+++ tsdat/config/attribute_defintion.py
@@ -1,7 +1,7 @@
 class AttributeDefinition:
     def __init__(self, name: str, value: str):
         self.name: str = name
-        self.value = value
+        self.value = value if value is not None else ""
     
     def to_dict(self):
         return {self.name, self.value}
