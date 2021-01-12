6a20141a89cf63351109ea9a6f4301c7a875c9ed
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-11 22:44:02 -0800

    Implemented VariableDefinition class

--- tsdat/config/variable_definition.py
+++ tsdat/config/variable_definition.py
@@ -9,18 +9,57 @@ class VarKeys:
     TYPE = 'type'
     ATTRS = 'attrs'
 
+class VarInputKeys:
+    NAME = 'name'
+    TIME_FORMAT = 'time_format'
+
+class VarInput:    
+    """-----------------------------------------------------------------------
+    Class to explicitly encode fields set by the variable's input source 
+    defined by the yaml file.
+    -----------------------------------------------------------------------"""
+    def __init__(self, dictionary: Dict):
+        self.name: str = dictionary[VarInputKeys.NAME]
+        self.time_format: str = dictionary.get(VarInputKeys.TIME_FORMAT, None)
+
 class VariableDefinition:
-    def __init__(self, name: str, dictionary: Dict):
-        self.name = name
-        self.input = dict.get("input", {})
-        self.attrs = dict.get('attrs', {})
-        self.dims = dict.get("dims", [])
-        self.type = dict.get("type", None)
-        for key in dict:
+    """-----------------------------------------------------------------------
+    Class to encode variable definitions from the config file. Also provides
+    a few utility methods.
+    -----------------------------------------------------------------------"""
+    def __init__(self, name: str, dictionary: Dict, available_dimensions: Dict[str, DimensionDefinition]):
+        self.name: str = name
+        self.input = self._parse_input(dictionary)
+        self.attrs = self._parse_attributes(dictionary)
+        self.dims = self._parse_dimensions(dictionary, available_dimensions)
+        self.type = self._parse_data_type(dictionary)
+
+        for key in dictionary:
             if not hasattr(self, key):
-                setattr(self, key, dict[key])
+                setattr(self, key, dictionary[key])
     
-    def _parse_data_type(self, data_type: str):
+    def _parse_input(self, dictionary: Dict) -> VarInput:
+        input_source = dictionary.get(VarKeys.INPUT, None)
+        if not input_source:
+            return None
+        return VarInput(input_source)
+    
+    def _parse_attributes(self, dictionary: Dict) -> Dict[str, AttributeDefinition]:
+        attributes: Dict[str, AttributeDefinition] = {}
+        for attr_name, attr_value in dictionary.get(VarKeys.ATTRS, {}).items():
+            attributes[attr_name] = AttributeDefinition(attr_name, attr_value)
+        return attributes
+
+    def _parse_dimensions(self, dictionary: Dict, available_dimensions: Dict[str, DimensionDefinition]) -> Dict[str, DimensionDefinition]:
+        requested_dimensions: List[str] = dictionary.get(VarKeys.DIMS, [])
+        parsed_dimensions: Dict[str, DimensionDefinition] = {}
+        for dim_name in requested_dimensions:
+            if dim_name not in available_dimensions:
+                raise KeyError(f"'{dim_name}' is not a recognized dimension. Available dimensions include: {', '.join(list(available_dimensions.keys()))}")
+            parsed_dimensions[dim_name] = available_dimensions[dim_name]
+        return parsed_dimensions
+
+    def _parse_data_type(self, dictionary: Dict) -> object:
         """-------------------------------------------------------------------
         Parses the data_type string and returns the appropriate numpy data 
         type (i.e. "float" -> np.float). 
@@ -34,6 +73,7 @@ class VariableDefinition:
                     is not in the MHKiT-Cloud Data Standards list of data 
                     types.
         -------------------------------------------------------------------"""
+        data_type: str = dictionary.get(VarKeys.TYPE, "")
         mappings = {
             "string":   np.str,
             "char":     np.str,
@@ -48,12 +88,9 @@ class VariableDefinition:
             "double":   np.float64
         }
         if data_type not in mappings:
-            error_message = f"{data_type} is not a standard data type. Data type must be one of: \n{'\n'.join(list(mappings.keys()))}"
+            error_message = f"'{data_type}' is not a standard data type. Data type must be one of: \n{', '.join(list(mappings.keys()))}"
             raise KeyError(error_message)
         return mappings[data_type]
-
-    def _parse_dimensions(self, dictionary: Dict) -> List[str]:
-        return dictionary.get("dims", [])
 
     def is_constant(self) -> bool:
         """-------------------------------------------------------------------
@@ -63,7 +100,7 @@ class VariableDefinition:
         Returns:
             bool: True if the variable is constant, False otherwise.
         -------------------------------------------------------------------"""
-        return len(self.dims) == 0
+        return len(list(self.dims.keys())) == 0
 
     def is_coordinate(self) -> bool:
         """-------------------------------------------------------------------
@@ -74,16 +111,39 @@ class VariableDefinition:
             bool:   True if the variable is a coordinate variable, False 
                     otherwise.
         -------------------------------------------------------------------"""
-        return self.name in [dim.name for dim in self.dims]
+        return [self.name] == [dim_name for dim_name in self.dims]
+
 
     def is_derived(self) -> bool:
-        """Return True if the variable is derived. A variable is derived if it does not have an input.
+        """-------------------------------------------------------------------
+        Return True if the variable is derived. A variable is derived if it 
+        does not have an input.
 
         Returns:
             bool: True if the Variable is derived, False otherwise.
-        """
-        return self.input == {}
+        -------------------------------------------------------------------"""
+        return self.input is not None
 
-    def get_units(self):
-        # TODO: return units as cfunits object
-        return self.attrs.get("units", 1)
+    def to_dict(self) -> Dict:
+        """-------------------------------------------------------------------
+        Returns the Variable as a dictionary to be used to intialize an xarray
+        Dataset or DataArray.
+
+        Returns a dictionary like (Example is for `temperature`):
+        ```
+        {
+            "dims": ["time"], 
+            "data": [], 
+            "attrs": {"units": "degC"}
+        }
+        ```
+
+        Returns:
+            Dict: A dictionary representation of the variable.
+        -------------------------------------------------------------------"""
+        dictionary = {
+            "dims":     [name for name, dim in self.dims.items()],
+            "data":     self.data if hasattr(self, "data") else [],
+            "attrs":    {attr_name: attr.value for attr_name, attr in self.attrs.items()}
+        }
+        return dictionary
