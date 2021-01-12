36a190d437d76c5bea3cd9b94c2af61922bea5b2
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-11 22:39:39 -0800

    Fixed dim length attribute and added better typing

--- tsdat/config/dimension_definition.py
+++ tsdat/config/dimension_definition.py
@@ -1,8 +1,11 @@
+class DimKeys:
+    LENGTH = 'length'
+
 
 class DimensionDefinition:
     def __init__(self, name: str, length):
-        self.name = name
-        self.length = length
+        self.name: str = name
+        self.length: str = length.get(DimKeys.LENGTH, None)
 
     def is_unlimited(self) -> bool:
         """Returns True is the dimension has unlimited length. Represented by
@@ -18,7 +21,6 @@ class DimensionDefinition:
         Returns True if the dimension has variable length, meaning that the
         dimension's length is set at runtime. Represented by setting the 
         length to "variable".
-
 
         Returns:
             bool: True if the dimension has variable length.
