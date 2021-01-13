28f29e1a99e0bb9a5531119d1fc50d8f081ecda3
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-12 16:06:42 -0800

    Updated pipeline testcase

--- tsdat/tests/test_pipeline.py
+++ tsdat/tests/test_pipeline.py
@@ -7,10 +7,10 @@ from tsdat.pipeline import IngestPipeline
 class TestIngestPipeline(unittest.TestCase):
 
     def test_ingest_pipeline(self):
-        config: Config = Config.load(["data/test1/pipeline.yml"])
-        storage: FilesystemStorage = FilesystemStorage("data/test1/outputs")
+        config: Config = Config.load("tsdat/tests/data/test1/ingest_pipeline.yml")
+        storage: FilesystemStorage = FilesystemStorage(root="tsdat/tests/data/test1/outputs")
         ingest: IngestPipeline = IngestPipeline(config, storage)
-        ingest.run("data/test1/buoy.z05.00.20200930.000000.temperature.csv")
+        ingest.run("tsdat/tests/data/test1/buoy.z05.00.20200930.000000.temperature.csv")
 
 if __name__ == '__main__':
     unittest.main()
