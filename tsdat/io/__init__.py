c12be3b6b735b8ec3f8b013bb8f03c358d8bbc03
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-14 18:13:02 -0800

    Initial commit

--- tsdat/io/__init__.py
+++ tsdat/io/__init__.py
@@ -1,1 +1,3 @@
-
+from .file_handlers import FileHandler
+from .storage import DatastreamStorage
+from .storage import FilesystemStorage
---
+++ tsdat/io/filehandlers/__init__.py
@@ -0,0 +1,5 @@
+from .file_handlers import AbstractFileHandler
+from .file_handlers import FileHandler
+from .file_handlers import register_filehandler
+from .csv_handler import CsvHandler
+from .netcdf_handler import NetCdfHandler
