599c096251b28c0ad43e547bf8e965d0edfd46be
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-14 18:11:29 -0800

    Removed unused imports

--- tsdat/config/config.py
+++ tsdat/config/config.py
@@ -3,9 +3,6 @@ import numpy as np
 import xarray as xr
 from typing import List, Dict
 from .keys import Keys
-from .attribute_defintion import AttributeDefinition
-from .dimension_definition import DimensionDefinition
-from .variable_definition import VariableDefinition
 from .dataset_definition import DatasetDefinition
 from .qctest_definition import QCTestDefinition
 
