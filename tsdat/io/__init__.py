7a06043d077bc44fdd6ff3f1f89619a61bae4478
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-18 12:24:01 -0800

    Improved imports

--- tsdat/io/__init__.py
+++ tsdat/io/__init__.py
@@ -1,3 +1,5 @@
-from .file_handlers import FileHandler
+from .filehandlers import AbstractFileHandler
+from .filehandlers import FileHandler
+from .filehandlers import register_filehandler
 from .storage import DatastreamStorage
 from .storage import FilesystemStorage
