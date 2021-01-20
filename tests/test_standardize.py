48867d19510544599690b66d2f7552d637bcfb62
Maxwell Levin <maxwell.levin@pnnl.gov>
2021-01-20 11:48:30 -0800

    Added additional test cases

--- tests/test_standardize.py
+++ tests/test_standardize.py
@@ -28,11 +28,21 @@ class TestStandardize(unittest.TestCase):
         pipeline = IngestPipeline(config=config, storage=None)
 
         # Now standardize our test data
-        pipeline.standardize(self.raw_ds)
+        self.std_ds = pipeline.standardize(self.raw_ds)
 
+        # Validate time conversion from raw
+        time_dt64 = np.array(self.std_ds["time"].data, dtype='datetime64[s]')
+        time_long = np.array(time_dt64, dtype=np.int64)
+        assert time_long[0] == 1601017205, "Error: timestamp is not correct"  # 2020-09-25T07:00:05
 
+        # Validate conversion of speed from km/s to m/s
+        raw_speed = self.raw_ds["HorizontalSpeedAverage(Double)"].data
+        std_speed = self.std_ds["HorizontalSpeedAverage"].data
+        assert np.all(raw_speed * 1e3 == std_speed), "Error: km/s to m/s conversion is not correct"
 
-        # TODO: validate the results
+        # Validate initialization of derived variable
+        assert np.all(self.std_ds["WindDistance"].data == -9999), "Error: derived variable was not initialized to _FillValue"
+        assert self.std_ds["WindDistance"].data.shape == self.std_ds["time"].shape, "Error: derived variable was not initialize with the correct shape"
 
 
 if __name__ == '__main__':
