53a9dcedddd5a2642a9bd94c54267b56c0750a2f
Maxwell <maxwellevin@users.noreply.github.com>
2021-03-10 16:16:23 -0800

    Updated waves ingest example

--- examples/a2e_waves_ingest/ingest.py
+++ examples/a2e_waves_ingest/ingest.py
@@ -1,34 +1,47 @@
 import os
 import shutil
+from tsdat.pipeline import IngestPipeline
 from tsdat import Config, FilesystemStorage
-from pipeline import CustomIngestPipeline
+from pipeline import WaveIngestPipeline
 
 # Folders where input data and configurations are stored
 example_dir = os.path.abspath(os.path.dirname(__file__))
 raw_dir = os.path.join(example_dir, 'storage/input')
 root_dir = os.path.join(example_dir, 'storage/root')
+try:
+    shutil.rmtree(root_dir)
+except:
+    pass
 os.makedirs(root_dir, exist_ok=True)
 
-def get_raw_file_copy(raw_filename):
+def get_raw_file_copy(raw_filename, data_input="data/input"):
     """-----------------------------------------------------------------------
     Copies the raw file into the temporary raw folder representing the pipeline
     input folder.  We need to do this because the pipeline will remove the
     processed file from the input folder if it completes with no error.
     -----------------------------------------------------------------------"""
-    file_to_copy = os.path.join(example_dir, "data/input", raw_filename)
+    file_to_copy = os.path.join(example_dir, data_input, raw_filename)
     pipeline_input_file = os.path.join(raw_dir, raw_filename)
     shutil.copy(file_to_copy, pipeline_input_file)
     return pipeline_input_file
 
 # Paths to the raw and config files used for this ingest
-raw_file = get_raw_file_copy('buoy.z05.00.20201117.000000.waves.csv')
+raw_file = get_raw_file_copy('buoy.z06.00.20201201.000000.waves.csv')
 config_file = os.path.join(example_dir, 'config.yml')
 
-# Create the necessary structures and run the ingest
+# Create necessary structures
 storage = FilesystemStorage(root_dir)
 config = Config.load(config_file)
-pipeline = CustomIngestPipeline(config, storage)
-pipeline.run(raw_file)
+pipeline = WaveIngestPipeline(config, storage)
 
-# Remove processed data
+# Run the ingest and remove the output if successful
+# Create the necessary structures and run the ingest
+pipeline.run(raw_file)
 shutil.rmtree(root_dir)
+
+# # Process all available data
+# for file in sorted(os.listdir(os.path.join(example_dir, "data/input/"))):
+#     # Ignore non-ingest files (Like .DS_Store or other system files)
+#     if file.endswith(".csv"):
+#         raw_file = get_raw_file_copy(file)
+#         pipeline.run(raw_file)
--- examples/a2e_waves_ingest/pipeline.py
+++ examples/a2e_waves_ingest/pipeline.py
@@ -1,117 +1,26 @@
-import act
+import os
+import cmocean
+import pandas as pd
 import xarray as xr
-import numpy as np
+import seaborn as sns
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-from typing import Dict
+
 from tsdat.pipeline import IngestPipeline
 from tsdat.utils import DSUtil
 
-class CustomIngestPipeline(IngestPipeline):
+example_dir = os.path.abspath(os.path.dirname(__file__))
+style_file = os.path.join(example_dir, "styling.mplstyle")
+plt.style.use(style_file)
+
+class WaveIngestPipeline(IngestPipeline):
     """-------------------------------------------------------------------
     This is an example class that extends the default IngestPipeline in
     order to hook in custom behavior such as creating custom plots.
     If users need to apply custom changes to the dataset, instrument
     corrections, or create custom plots, they should follow this example
     to extend the IngestPipeline class.
-    -------------------------------------------------------------------"""
-    def customize_raw_datasets(self, raw_dataset_mapping: Dict[str, xr.Dataset]) -> Dict[str, xr.Dataset]:
-        """-------------------------------------------------------------------
-        Hook to allow for user customizations to one or more raw xarray Datasets
-        before they merged and used to create the standardized dataset.  The
-        raw_dataset_mapping will contain one entry for each file being used
-        as input to the pipeline.  The keys are the standardized raw file name,
-        and the values are the datasets.
-
-        This method would typically only be used if the user is combining
-        multiple files into a single dataset.  In this case, this method may
-        be used to correct coordinates if they don't match for all the files,
-        or to change variable (column) names if two files have the same
-        name for a variable, but they are two distinct variables.
-
-        This method can also be used to check for unique conditions in the raw
-        data that should cause a pipeline failure if they are not met.
-
-        This method is called before the inputs are merged and converted to
-        standard format as specified by the config file.
-
-        Args:
-            raw_dataset_mapping (Dict[str, xr.Dataset])     The raw datasets to
-                                                            customize.
-
-        Returns:
-            Dict[str, xr.Dataset]: The customized raw dataset.
-        -------------------------------------------------------------------"""
-        # In this hook we rename one variable from the surfacetemp file to
-        # prevent a naming conflict with a variable in the conductivity file.
-        for filename, dataset in raw_dataset_mapping.items():
-            if "surfacetemp" in filename: 
-                old_name = "Surface Temperature (C)"
-                new_name = "surfacetemp - Surface Temperature (C)"
-                raw_dataset_mapping[filename] = dataset.rename_vars({old_name: new_name})
-
-            if "gill" in filename:
-                name_mapping = {
-                    "Horizontal Speed (m/s)":       "gill_horizontal_wind_speed",
-                    "Horizontal Direction (deg)":   "gill_horizontal_wind_direction" 
-                }
-                raw_dataset_mapping[filename] = dataset.rename_vars(name_mapping)
-
-        # No customization to raw data - return original dataset
-        return raw_dataset_mapping
-
-    def apply_corrections(self, dataset: xr.Dataset, raw_dataset_mapping: Dict[str, xr.Dataset]) -> xr.Dataset:
-        """-------------------------------------------------------------------
-        Pipeline hook that can be used to apply standard corrections for the
-        instrument/measurement or calibrations. This method is called
-        immediately after the dataset is converted to standard format and
-        before any QC tests are applied.
-
-        If corrections are applied, then the `corrections_applied` attribute
-        should be updated on the variable(s) that this method applies
-        corrections to.
-
-        Args:
-            dataset (xr.Dataset):   A standardized xarray dataset where the
-                                    variable names correspond with the output
-                                    variable names from the config file.
-        Returns:
-            xr.Dataset: The input xarray dataset with corrections applied.
-        -------------------------------------------------------------------"""
-        # No corrections - return the original dataset
-        return dataset
-
-    def customize_dataset(self, dataset: xr.Dataset, raw_dataset_mapping: Dict[str, xr.Dataset]) -> xr.Dataset:
-        """-------------------------------------------------------------------
-        Hook to allow for user customizations to the standardized dataset such
-        as inserting a derived variable based on other variables in the
-        dataset.  This method is called immediately after the apply_corrections
-        hook and before any QC tests are applied.
-
-        Args:
-            dataset (xr.Dataset): The dataset to customize.
-
-        Returns:
-            xr.Dataset: The customized dataset.
-        -------------------------------------------------------------------"""
-        for raw_filename, raw_dataset in raw_dataset_mapping.items():
-            if "currents" in raw_filename:
-                num_bins = 50
-                # Want to add Vel1 (mm/s), Vel2 (mm/s), Vel3 (mm/s), ... to the 
-                # current_velocity variable so that it is two dimensional.
-                vel_names = [f"Vel{i} (mm/s)" for i in range(1, num_bins + 1)]
-                vel_data  = [raw_dataset[name].data for name in vel_names]
-                vel_data  = np.array(vel_data).transpose()
-                dataset["current_velocity"].data = vel_data
-
-                # Want to add Dir1 (deg), Dir2 (deg), Dir3 (deg), ... to the 
-                # current_direction variable so that it is two dimensional.
-                vel_names = [f"Dir{i} (deg)" for i in range(1, num_bins + 1)]
-                vel_data  = [raw_dataset[name].data for name in vel_names]
-                vel_data  = np.array(vel_data).transpose()
-                dataset["current_direction"].data = vel_data
-        
-        return dataset
-    
+    -------------------------------------------------------------------"""    
     def create_and_persist_plots(self, dataset: xr.Dataset) -> None:
         """-------------------------------------------------------------------
         Hook to allow users to create plots from the xarray dataset after
@@ -128,8 +37,9 @@ class CustomIngestPipeline(IngestPipeline):
         with self.storage._tmp.get_temp_filepath(filename) as tmp_path:
             fig, ax = plt.subplots(figsize=(10,5))
             ax.plot(dataset["time"].data, dataset["sea_level"].data)
-            fig.save(tmp_path)
-            storage.save(tmp_path)
+            fig.savefig(tmp_path)
+            self.storage.save(tmp_path)
+            plt.close()
 
         filename = DSUtil.get_plot_filename(dataset, "qc_sea_level", "png")
         with self.storage._tmp.get_temp_filepath(filename) as tmp_path:
@@ -142,19 +52,55 @@ class CustomIngestPipeline(IngestPipeline):
             dataset (xr.Dataset):   The xarray dataset with customizations and
                                     QC applied.
         -------------------------------------------------------------------"""
-        for variable_name in dataset.data_vars.keys():
-            if variable_name.startswith("qc_") or "time" not in dataset[variable_name].dims:
-                continue
-            filename = DSUtil.get_plot_filename(dataset, variable_name, "png")
-            with self.storage._tmp.get_temp_filepath(filename) as tmp_path:
+        def format_time_xticks(ax, /, *, start=4, stop=21, step=4, date_format="%H-%M"):
+            ax.xaxis.set_major_locator(mpl.dates.HourLocator(byhour=range(start, stop, step)))
+            ax.xaxis.set_major_formatter(mpl.dates.DateFormatter(date_format))
+            plt.setp(ax.xaxis.get_majorticklabels(), rotation=0)
+        
+        # Useful variables
+        ds = dataset
+        date = pd.to_datetime(ds.time.data[0]).strftime('%d-%b-%Y')
 
-                display = act.plotting.TimeSeriesDisplay(dataset, subplot_shape=(2,), figsize=(15,9), sharex=True)
-                display.plot(variable_name, subplot_index=(0,))
-                display.qc_flag_block_plot(variable_name, subplot_index=(1,))
-                display.fig.savefig(tmp_path)
-                plt.close()
+        # Create wave statistics plot
+        filename = DSUtil.get_plot_filename(dataset, "wave_statistics", "png")
+        with self.storage._tmp.get_temp_filepath(filename) as tmp_path:
 
-                self.storage.save(tmp_path)
+            # Create figure and axes objects
+            fig, axs = plt.subplots(nrows=3, figsize=(14, 8), constrained_layout=True)
+            fig.suptitle(f"Wave Statistics at {ds.attrs['location_meaning']} on {date}")
+
+            # Plot wave heights
+            cmap = cmocean.cm.amp_r
+            ds.average_wave_height.plot(ax=axs[0], c=cmap(0.15), linewidth=2, label=r"H$_{avg}$")
+            ds.significant_wave_height.plot(ax=axs[0], c=cmap(0.4), linewidth=2, label=r"H$_{sig}$")
+            ds.max_wave_height.plot(ax=axs[0], c=cmap(0.65), linewidth=2, label=r"H$_{max}$")
+            axs[0].set_ylabel("Wave Height (m)")
+            axs[0].legend(loc=1, ncol=3)
+
+            # Plot wave periods
+            cmap = cmocean.cm.dense
+            ds.average_wave_period.plot(ax=axs[1], c=cmap(0.15), linewidth=2, label=r"T$_{avg}$")
+            ds.significant_wave_period.plot(ax=axs[1], c=cmap(0.5), linewidth=2, label=r"T$_{sig}$")
+            ds.mean_wave_period.plot(ax=axs[1], c=cmap(0.8), linewidth=2, label=r"$\overline{T}_{mean}$")
+            axs[1].set_ylabel("Wave Period (s)")
+            axs[1].legend(loc=1, ncol=3)
+
+            # Plot mean period and direction
+            cmap = cmocean.cm.haline
+            ds.mean_wave_direction.plot(ax=axs[2], c=cmap(0.4), linewidth=2, label=r"$\overline{\phi}_{mean}$")
+            axs[2].set_ylabel(r"Wave $\overline{\phi}$ (deg)")
+            axs[2].legend(loc=1)
+
+            # Set xlabels and ticks
+            for i in range(3):
+                axs[i].set_xlabel("Time (UTC)")
+                format_time_xticks(axs[i])
+            
+            # Save figure
+            fig.savefig(tmp_path)
+            self.storage.save(tmp_path)
+            plt.close()
 
         return
+
 
