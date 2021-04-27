41cdbd916630f415d3bfa0007f2d15655d6d9a17
Maxwell <maxwellevin@users.noreply.github.com>
2021-04-27 15:00:01 -0700

    Updated CsvHandler to use config parameters

--- tsdat/io/filehandlers/csv_handler.py
+++ tsdat/io/filehandlers/csv_handler.py
@@ -10,52 +10,36 @@ from .file_handlers import AbstractFileHandler
 class CsvHandler(AbstractFileHandler):
 
     def write(self, ds: xr.Dataset, filename: str, config: Config = None, **kwargs):
-        # You can only write one-dimensional data to csv
         if len(ds.dims) > 1:
             raise TypeError("Dataset has more than one dimension, so it can't be saved to csv.  Try netcdf instead.")
 
-        # First convert the data to a Pandas DataFrame and
-        # save the variable metadata in a dictionary
-        variables = {}
-        df: pd.DataFrame = pd.DataFrame()
-        for variable_name in ds.variables:
-            variable = ds[variable_name]
-            df[variable_name] = variable.to_pandas()
-            dictionary = {
-                "dims":     [name for name, dim in variable.dims.items()],
-                "attrs":    {attr_name: attr.value for attr_name, attr in variable.attrs.items()}
-            }
-            variables[variable_name] = dictionary
+        write_params = self.parameters.get('write', {})
+        to_dataframe_kwargs = write_params.get('to_dataframe', {})
+        to_csv_kwargs = dict(index=False)
+        to_csv_kwargs.update(write_params.get('to_csv', {}))
 
-        # Then save the DataFrame to a csv file
-        kwargs['index'] = False
-        df.to_csv(filename, **kwargs)
+        df = ds.to_dataframe(**to_dataframe_kwargs)
+        df.to_csv(filename, **to_csv_kwargs)
 
-        # Now save all the metadata dictionary to a companion yaml file
-        metadata = {"variables": variables}
         yaml_filename = f"{filename}.yaml"
         with open(yaml_filename, 'w') as file:
+            metadata = DSUtil.get_metadata(ds)
             yaml.dump(metadata, file)
 
-    def read(self, filename: str, **kwargs):
-        # First read the csv into a pandas dataframe
-        # dataset_def = DSUTIL.extract_metadata(ds)
-        # ds = DSUTIL.set_metadata(ds, dataset_definition)
-        # or could live in Config/DatasetDefiniton (Or both)
-        dataframe: pd.DataFrame = pd.read_csv(filename, **kwargs)
+    def read(self, filename: str, **kwargs) -> xr.Dataset:
+        read_params = self.parameters.get('read', {})
+        read_csv_kwargs = read_params.get('read_csv', {})
+        to_xarray_kwargs = read_params.get('to_xarray', {})
 
-        # Now see if there is an accompanying metadata file.  If so,
-        # then merge those attributes into the config
+        df = pd.read_csv(filename, **read_csv_kwargs)
+        ds: xr.Dataset = df.to_xarray(**to_xarray_kwargs)
+
         yaml_filename = f"{filename}.yaml"
-        if os.path.exists(yaml_filename):
-            dict = {}
-
+        if os.path.isfile(yaml_filename):
             with open(yaml_filename, 'r') as file:
-                new_dict = yaml.safe_load(file)
-                dict.update(new_dict)
+                metadata = yaml.safe_load(file)
+                ds.attrs = metadata.get('attributes', {})
+                for variable, attrs in metadata.get('variables', {}).items():
+                    ds[variable].attrs = attrs
 
-            config = Config(new_dict)
-
-        # TODO: Use config?
-        return xr.Dataset(dataframe.to_xarray())
-
+        return ds
