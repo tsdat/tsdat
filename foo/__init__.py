89483575da52158f93ca55e3a36d80a0ef7c484c
Maxwell <maxwellevin@users.noreply.github.com>
2021-05-05 12:49:43 -0700

    Removed misplaced header

--- foo/__init__.py
+++ foo/__init__.py
@@ -1,7 +1,4 @@
 """
-What is foo?
-^^^^^^^^^^^^
-
 foo auto-generates API documentation that follows your project's Python module hierarchy.
 It can be used as a command-line application as well as a library.
 *pdoc the command-line application* can be used to render a project's
