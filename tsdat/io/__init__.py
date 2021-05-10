b8734af9a00bea48ea242f4913f6d92e4759eb21
Maxwell <maxwellevin@users.noreply.github.com>
2021-05-10 09:59:09 -0700

    Re-organized imports

--- tsdat/io/__init__.py
+++ tsdat/io/__init__.py
@@ -1,7 +1,12 @@
 from .filehandlers import AbstractFileHandler
 from .filehandlers import FileHandler
 from .filehandlers import register_filehandler
-from .storage import DatastreamStorage, TemporaryStorage, DisposableLocalTempFile, DisposableStorageTempFileList, \
+from .storage import (
+    DatastreamStorage, 
+    TemporaryStorage, 
+    DisposableLocalTempFile, 
+    DisposableStorageTempFileList,
     DisposableLocalTempFileList
+    )
 from .filesystem_storage import FilesystemStorage
 from .aws_storage import AwsStorage, S3Path
