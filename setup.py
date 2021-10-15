b82cfd077e71fb3d38b351015619730046c4192a
Maxwell Levin <maxwellevin@users.noreply.github.com>
2021-10-14 21:28:09 -0700

    Semi-automate versioning and release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from os import path
+import os
 import pathlib
-from setuptools import setup, find_packages
+import setuptools
 
 # The directory containing this file
 CWD = pathlib.Path(__file__).parent
@@ -9,7 +9,7 @@ CWD = pathlib.Path(__file__).parent
 README = (CWD / "README.md").read_text()
 
 # Get the list of dependencies from the requirements.txt file
-with open(path.join(CWD, "requirements.txt")) as requirements_file:
+with open(os.path.join(CWD, "requirements.txt")) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
     REQUIREMENTS = [
         line
@@ -17,26 +17,31 @@ with open(path.join(CWD, "requirements.txt")) as requirements_file:
         if not line.startswith("#")
     ]
 
-setup(
+version = os.environ["TSDAT_VERSION"]
+assert "." in version
+
+setuptools.setup(
     name="tsdat",
-    version="0.2.6",
+    version=version,
     description="A data processing framework used to convert time series data into standardized format.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/tsdat/tsdat",
-    author="Carina Lansing <carina.lansing@pnnl.gov>, Maxwell Levin <maxwell.levin@pnnl.gov>",
-    author_email="carina.lansing@pnnl.gov",
+    author="Carina Lansing, Maxwell Levin",
+    author_email="tsdat@pnnl.gov",
     license="Simplified BSD (2-clause)",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
     ],
-    packages=find_packages(exclude=["docs", "tests", "examples", "examples_dev"]),
+    packages=setuptools.find_packages(exclude=["docs", "tests", "examples"]),
     entry_points={"console_scripts": []},
     include_package_data=True,
     zip_safe=False,
