eb19005de5b782455c8b49e68424d467000fdfdb
Maxwell Levin <maxwellevin@users.noreply.github.com>
2022-02-01 10:45:16 -0800

    Added docstrings, made variables private

--- tsdat/io/handlers/archive.py
+++ tsdat/io/handlers/archive.py
@@ -10,22 +10,62 @@ from .handlers import DataHandler, HandlerRegistry
 
 
 class ArchiveHandler(DataHandler):
+    """------------------------------------------------------------------------------------
+    Base class for DataHandler objects that read data from archives.
 
-    registry: HandlerRegistry = None
-    handlers: List[DataHandler] = None
-    exclude: str = None
+    This class, and any classes derived from it, require a `handlers` section in the
+    parameters given to this `DataHandler` from the storage configuration file. The
+    structure of the `handlers` section underneath an `ArchiveHandler` should mirror the
+    structure of its parent `handlers` section. To illustrate, consider the following
+    configuration block for the `ZipHandler` class:
+
+    .. code-block:: yaml
+
+        file_handlers:
+          input:
+            zip:
+              file_pattern: '.*\\.zip'
+              classname: "tsdat.io.handlers.ZipHandler"
+              parameters:
+                # The handlers section tells the ArchiveHandler which DataHandlers should be
+                # used to handle the unpacked files.
+                handlers:
+                  csv:
+                    file_pattern: '.*\\.csv'
+                    classname: tsdat.io.handlers.CsvHandler
+                    parameters:  # Parameters specific to tsdat.io.handlers.CsvHandler
+                      sep: '\\t'
+                # Pattern(s) used to exclude certain files in the archive from being handled. This
+                # parameter is optional, and the default value is shown below:
+                exclude: ['.*\\_\\_MACOSX/.*', '.*\\.DS_Store']
+
+    All `ArchiveHandler` classes also allow an optional `exclude` parameter, which is used
+    to filter out unpacked files matching regex patterns defined by the `exclude`
+    parameter.
+
+    Subclasses of `ArchiveHandler` may define additional parameters to support various
+    methods of unpacking archived data.
+
+    ------------------------------------------------------------------------------------"""
+
+    _registry: HandlerRegistry = None
+
+    _handlers: List[DataHandler] = None
+    """A list of DataHandlers that the ArchiveHandler should use."""
+
+    _exclude: str = None
 
     def __init__(self, parameters: Dict = None):
-        # TODO: Validate parameters
+
         super().__init__(parameters=parameters)
 
-        self.registry = HandlerRegistry()
+        self._registry = HandlerRegistry()
 
-        self.handlers = list()
+        self._handlers = list()
         for handler_dict in self.parameters["handlers"].values():
             child: DataHandler = instantiate_handler(handler_desc=handler_dict)
-            self.handlers.append(child)
-            self.registry.register_file_handler(
+            self._handlers.append(child)
+            self._registry.register_file_handler(
                 method="read",
                 patterns=handler_dict["file_pattern"],
                 handler=child,
@@ -35,10 +75,55 @@ class ArchiveHandler(DataHandler):
         # read. By default we exclude files that macOS creates when zipping a folder.
         exclude = [".*\\_\\_MACOSX/.*", ".*\\.DS_Store"]
         exclude.extend(self.parameters.get("exclude", []))
-        self.exclude = "(?:% s)" % "|".join(exclude)
+        self._exclude = "(?:% s)" % "|".join(exclude)
 
 
 class TarHandler(ArchiveHandler):
+    """------------------------------------------------------------------------------------
+    DataHandler for reading from a tarred archive. Writing to this format is not supported.
+
+    This class requires a that `handlers` be specified in the parameters section of the
+    storage configuration file. The structure of the `handlers` section should mirror the
+    structure of its parent `handlers` section. To illustrate, consider the following
+    configuration block:
+
+    .. code-block:: yaml
+
+        file_handlers:
+          input:
+            tar:
+              file_pattern: '.*\\.tar'
+              classname: "tsdat.io.handlers.TarHandler"
+              parameters:
+                # Parameters to specify how the TarHandler should read/unpack the archive.
+                read:
+                  # Parameters here are passed to the Python open() method as kwargs. The
+                  # default value is shown below.
+                  open:
+                    mode: "rb"
+
+                  # Parameters here are passed to tarfile.open() as kwargs. Useful for
+                  # specifying the system encoding or compression algorithm to use for
+                  # unpacking the archive. These are optional.
+                  # tarfile:
+                    # mode: "r:gz"
+
+
+                # The handlers section tells the TarHandler which DataHandlers should be
+                # used to handle the unpacked files.
+                handlers:
+                  csv:
+                    file_pattern: '.*\\.csv'
+                    classname: tsdat.io.handlers.CsvHandler
+                    parameters:  # Parameters specific to tsdat.io.handlers.CsvHandler
+                      sep: '\\t'
+
+                # Pattern(s) used to exclude certain files in the archive from being handled.
+                # This parameter is optional, and the default value is shown below:
+                exclude: ['.*\\_\\_MACOSX/.*', '.*\\.DS_Store']
+
+    ------------------------------------------------------------------------------------"""
+
     def read(
         self,
         file: Union[str, BytesIO],
@@ -88,10 +173,10 @@ class TarHandler(ArchiveHandler):
 
             file_label = f"{label}::{filename}"
 
-            if re.match(self.exclude, filename):
+            if re.match(self._exclude, filename):
                 continue
 
-            handler: DataHandler = self.registry._get_handler(
+            handler: DataHandler = self._registry._get_handler(
                 name=filename,
                 method="read",
             )
@@ -109,6 +194,51 @@ class TarHandler(ArchiveHandler):
 
 
 class ZipHandler(ArchiveHandler):
+    """------------------------------------------------------------------------------------
+    DataHandler for reading from a zipped archive. Writing to this format is not supported.
+
+    This class requires a that `handlers` be specified in the parameters section of the
+    storage configuration file. The structure of the `handlers` section should mirror the
+    structure of its parent `handlers` section. To illustrate, consider the following
+    configuration block:
+
+    .. code-block:: yaml
+
+        file_handlers:
+          input:
+            zip:
+              file_pattern: '.*\\.zip'
+              classname: "tsdat.io.handlers.ZipHandler"
+              parameters:
+                # Parameters to specify how the ZipHandler should read/unpack the archive.
+                read:
+                  # Parameters here are passed to the Python open() method as kwargs. The
+                  # default value is shown below.
+                  open:
+                    mode: "rb"
+
+                  # Parameters here are passed to zipfile.ZipFile.open() as kwargs. Useful
+                  # for specifying the system encoding or compression algorithm to use for
+                  # unpacking the archive. These are optional.
+                  # zipfile:
+                    # mode: "r"
+
+
+                # The handlers section tells the ZipHandler which DataHandlers should be
+                # used to handle the unpacked files.
+                handlers:
+                  csv:
+                    file_pattern: '.*\\.csv'
+                    classname: tsdat.io.handlers.CsvHandler
+                    parameters:  # Parameters specific to tsdat.io.handlers.CsvHandler
+                      sep: '\\t'
+
+                # Pattern(s) used to exclude certain files in the archive from being handled.
+                # This parameter is optional, and the default value is shown below:
+                exclude: ['.*\\_\\_MACOSX/.*', '.*\\.DS_Store']
+
+    ------------------------------------------------------------------------------------"""
+
     def read(
         self,
         file: Union[str, BytesIO],
@@ -156,10 +286,10 @@ class ZipHandler(ArchiveHandler):
         for filename in zip.namelist():
             file_label = f"{label}::{filename}"
 
-            if re.match(self.exclude, filename):
+            if re.match(self._exclude, filename):
                 continue
 
-            handler: DataHandler = self.registry._get_handler(
+            handler: DataHandler = self._registry._get_handler(
                 name=filename,
                 method="read",
             )
