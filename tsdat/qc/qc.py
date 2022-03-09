f9c9842ace4e329c166765a349320b2291b8abf4
Maxwell Levin <maxwellevin@users.noreply.github.com>
2022-03-09 12:47:28 -0800

    Stub out QC logic

--- tsdat/qc/qc.py
+++ tsdat/qc/qc.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List
 from pydantic import BaseModel, Extra
 
+from tsdat.config.dataset import DatasetConfig
+
 
 class BaseQualityChecker(BaseModel, ABC, extra=Extra.forbid):
     parameters: Dict[str, Any] = {}
@@ -30,7 +32,12 @@ class QualityManager(BaseModel, extra=Extra.forbid):
     apply_to: List[str]
     exclude: List[str]
 
-    def run(self, dataset: xr.Dataset) -> xr.Dataset:
+    def run(self, dataset: xr.Dataset, dataset_config: DatasetConfig) -> xr.Dataset:
+        # TODO:
+        # 1. Resolve the list of variables to run
+        # 2. Call self.QualityChecker.run() with appropriate args
+        # 3. Iteratively call self.QualityHandler[X].run() with appropriate args
+        # 4. Return output dataset
         ...
 
 
@@ -39,3 +46,9 @@ class QualityRegistry(BaseModel, extra=Extra.forbid):
     # users to customize how quality dispatch is done (e.g., to support parallel
     # dispatch, or other custom rulesets).
     managers: List[QualityManager]
+
+    def manage(self, dataset: xr.Dataset, dataset_config: DatasetConfig) -> xr.Dataset:
+        output = dataset
+        for manager in self.managers:
+            output = manager.run(output, dataset_config)
+        return output
