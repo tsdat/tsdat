9e4cebdd4a85a80df36c226d76e9dc1c0ad85cc2
Maxwell Levin <maxwellevin@users.noreply.github.com>
2022-09-21 19:14:07 -0700

    Add 2D storage retriever test

--- test/io/data/retriever-store/data.py
+++ test/io/data/retriever-store/data.py
@@ -3,10 +3,12 @@
 # tests.
 #
 
-from pathlib import Path
+from enum import Enum
 import pandas as pd
 import xarray as xr
+from pathlib import Path
 
+from typer import Typer, Option
 
 base_path = Path("test/io/data/retriever-store/data/")
 
@@ -38,12 +40,72 @@ dataset2 = xr.Dataset(
     attrs={"datastream": "humboldt.buoy_z07.a1"},
 )
 
-dataset1.to_netcdf(base_path / "humboldt.buoy_z06.a1/humboldt.buoy_z06.a1.20220405.000000.nc")  # type: ignore
-dataset2.to_netcdf(base_path / "humboldt.buoy_z07.a1/humboldt.buoy_z07.a1.20220405.000000.nc")  # type: ignore
 
 #
 # Second test: Multidimensional datasets (time, height)
+#   - tests will take time from dataset 1 and height from dataset 2
 #
 
-# TODO
-# dataset1_2D = xr.Dataset(coords={"time": ("time",)})
+dataset1_2D = xr.Dataset(
+    coords={
+        "time": ("time", time_3pt),
+        "height": ("height", [1.0, 4.0, 11.0, 17.0], {"units": "m"}),
+    },
+    data_vars={
+        "temp": (
+            ("time", "height"),
+            [[70, 72, 74, 76], [80, 82, 84, 86], [90, 92, 94, 96]],
+        ),
+        "humidity": ("time", [0, 10, 20]),
+        "pres": ("height", [15, 10, 5, 0]),
+    },
+    attrs={"datastream": "humboldt.buoy_z06.a1"},
+)
+
+dataset2_2D = xr.Dataset(
+    coords={
+        "time": ("time", time_10pt),
+        "height": ("height", [0.0, 5.0, 10.0], {"units": "m"}),
+    },
+    data_vars={
+        "temp": (
+            ("time", "height"),
+            [
+                [0, 2, 4],
+                [6, 8, 10],
+                [12, 14, 16],
+                [18, 20, 22],
+                [24, 26, 28],
+                [30, 32, 34],
+                [36, 38, 40],
+                [42, 44, 46],
+                [48, 50, 52],
+                [54, 56, 58],
+            ],
+        ),
+        "rh": ("time", [40, 45, 50, 55, 60, 65, 70, 75, 80, 85]),
+        # "pres": ("height", [14, 12, 10]),
+    },
+    attrs={"datastream": "humboldt.buoy_z07.a1"},
+)
+
+app = Typer()
+
+
+class DataVersion(str, Enum):
+    ONE = "1"
+    TWO = "2"
+
+
+@app.command()
+def generate(data_version: DataVersion = Option(...)):
+    if data_version == DataVersion.ONE:
+        dataset1.to_netcdf(base_path / "humboldt.buoy_z06.a1/humboldt.buoy_z06.a1.20220405.000000.nc")  # type: ignore
+        dataset2.to_netcdf(base_path / "humboldt.buoy_z07.a1/humboldt.buoy_z07.a1.20220405.000000.nc")  # type: ignore
+    elif data_version == DataVersion.TWO:
+        dataset1_2D.to_netcdf(base_path / "humboldt.buoy_z06_2D.a1/humboldt.buoy_z06-2D.a1.20220405.000000.nc")  # type: ignore
+        dataset2_2D.to_netcdf(base_path / "humboldt.buoy_z07_2D.a1/humboldt.buoy_z07-2D.a1.20220405.000000.nc")  # type: ignore
+
+
+if __name__ == "__main__":
+    app()
--- test/io/test_retrievers.py
+++ test/io/test_retrievers.py
@@ -13,6 +13,13 @@ from tsdat import (
     assert_close,
 )
 
+# Coords used in sample input data
+time_3pt = pd.date_range("2022-04-05", "2022-04-06", periods=3 + 1, inclusive="left")  # type: ignore
+time_10pt = pd.date_range("2022-04-05", "2022-04-06", periods=10 + 1, inclusive="left")  # type: ignore
+
+height_3pt = [0.0, 5.0, 10.0]
+height_4pt = [1.0, 4.0, 11.0, 17.0]
+
 
 @fixture
 def simple_retriever() -> DefaultRetriever:
@@ -27,8 +34,19 @@ def storage_retriever() -> StorageRetriever:
 
 
 @fixture
+def storage_retriever_2D() -> StorageRetriever:
+    config = RetrieverConfig.from_yaml(Path("test/io/yaml/vap-retriever-2D.yaml"))
+    return recursive_instantiate(config)
+
+
+@fixture
 def vap_dataset_config() -> DatasetConfig:
     return DatasetConfig.from_yaml(Path("test/io/yaml/vap-dataset.yaml"))
+
+
+@fixture
+def vap_dataset_config_2D() -> DatasetConfig:
+    return DatasetConfig.from_yaml(Path("test/io/yaml/vap-dataset-2D.yaml"))
 
 
 @fixture
@@ -90,7 +108,6 @@ def test_simple_extract_multifile_dataset(
 def test_storage_retriever(
     storage_retriever: StorageRetriever, vap_dataset_config: DatasetConfig
 ):
-    # TODO: Test with some 2D data
     storage = FileSystem(
         parameters=FileSystem.Parameters(
             storage_root=Path("test/io/data/retriever-store")
@@ -121,6 +138,38 @@ def test_storage_retriever(
     xr.testing.assert_allclose(retrieved_dataset, expected)  # type: ignore
 
 
-# TEST: Multiple input datasets with non-overlapping or partially-overlapping data vars
-# def test_multi_datastream_retrieval():
-#     pass
+def test_storage_retriever_2D(
+    storage_retriever_2D: StorageRetriever, vap_dataset_config_2D: DatasetConfig
+):
+    storage = FileSystem(
+        parameters=FileSystem.Parameters(
+            storage_root=Path("test/io/data/retriever-store")
+        )
+    )
+    inputs = [
+        "humboldt.buoy_z06-2D.a1::20220405.000000::20220406.000000",
+        "humboldt.buoy_z07-2D.a1::20220405.000000::20220406.000000",
+    ]
+
+    retrieved_dataset = storage_retriever_2D.retrieve(
+        inputs, dataset_config=vap_dataset_config_2D, storage=storage
+    )
+
+    expected = xr.Dataset(
+        coords={
+            "time": ("time", time_3pt),
+            "height": ("height", height_3pt),
+        },
+        data_vars={
+            "temperature": (  # degF -> degC
+                ("time", "height"),
+                (np.array([[0, 2, 4], [18, 20, 22], [42, 44, 46]]) - 32) * 5 / 9,
+                {"units": "degC"},
+            ),
+            "humidity": ("time", [0, 10, 20]),
+            "pressure": ("height", [15, 10, 5]),
+        },
+        attrs={"datastream": "humboldt.buoy.b1"},
+    )
+
+    xr.testing.assert_allclose(retrieved_dataset, expected)  # type: ignore
