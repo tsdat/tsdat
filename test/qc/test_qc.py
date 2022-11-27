de3112fa37dd2e823349533f034cb8ec1c4654af
Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>
2022-11-27 13:34:41 -0800

    Test CheckMonotonic on string data types

--- test/qc/test_qc.py
+++ test/qc/test_qc.py
@@ -173,6 +173,21 @@ def test_valid_delta():
     assert np.array_equal(results, expected)  # type: ignore
 
 
+def test_monotonic_check_ignores_string_vars(capsys: pytest.CaptureFixture[str]):
+    ds = xr.Dataset(
+        coords={
+            "time": pd.date_range("2022-03-24 21:43:00", "2022-03-24 21:45:00", periods=3),  # type: ignore
+            "dir": ["N", "E", "S", "W"],
+        },
+        data_vars={
+            "wind_speed": (["time", "dir"], np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))  # type: ignore
+        },
+    )
+    expected = np.array([False, False, False, False])
+    results = CheckMonotonic().run(ds, "dir")  # type: ignore
+    assert np.array_equal(results, expected)  # type: ignore
+
+
 def test_check_delta_classes(sample_dataset: xr.Dataset):
     var_name = "monotonic_var"
     expected = np.bool8([False, False, False, True])
