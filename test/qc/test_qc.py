8aa73a2e94258f4b4cb7cf6ca4c582be890db007
Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>
2022-11-27 12:36:23 -0800

    Add 2D test for CheckValidDelta

--- test/qc/test_qc.py
+++ test/qc/test_qc.py
@@ -158,6 +158,21 @@ def test_check_max_classes(sample_dataset: xr.Dataset):
     assert np.array_equal(results, expected)  # type: ignore
 
 
+def test_valid_delta():
+    ds = xr.Dataset(
+        coords={
+            "time": pd.date_range("2022-03-24 21:43:00", "2022-03-24 21:45:00", periods=3),  # type: ignore
+            "height": np.array([1, 2]),
+        },
+        data_vars={
+            "wind_speed": (["time", "height"], np.array([[10, 20], [11, 25], [16, 31]]), {"units": "m/s", "valid_delta": 5})  # type: ignore
+        },
+    )
+    expected = np.array([[False, False], [False, False], [False, True]])
+    results = CheckValidDelta().run(ds, "wind_speed")
+    assert np.array_equal(results, expected)  # type: ignore
+
+
 def test_check_delta_classes(sample_dataset: xr.Dataset):
     var_name = "monotonic_var"
     expected = np.bool8([False, False, False, True])
