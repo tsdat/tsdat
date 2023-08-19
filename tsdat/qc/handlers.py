2cc8addee3314ee6d2c947b08f1e6d01063672f6
James McVey <53623232+jmcvey3@users.noreply.github.com>
2023-08-18 18:13:28 -0700

    Update QC Functions (#140)
    * Fixes
    
    * Update test for improved checker
    
    * Pin older version of pydantic
    
    * Pydantic 1.9 also doesn't work
    
    * Workaround for weird setup-miniconda issue
    
    * Attempt #2
    
    * Test with python 3.10
    
    ---------
    
    Co-authored-by: Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>

--- tsdat/qc/handlers.py
+++ tsdat/qc/handlers.py
@@ -175,8 +175,12 @@ class RemoveFailedValues(QualityHandler):
         self, dataset: xr.Dataset, variable_name: str, failures: NDArray[np.bool_]
     ) -> xr.Dataset:
         if failures.any():
-            fill_value = dataset[variable_name].attrs.get("_FillValue", None)
-            dataset[variable_name] = dataset[variable_name].where(~failures, fill_value)  # type: ignore
+            if variable_name in dataset.dims:
+                mask = xr.DataArray(failures, coords={variable_name:dataset[variable_name]})
+                dataset = dataset.where(~mask, drop=True)
+            else:
+                fill_value = dataset[variable_name].attrs.get("_FillValue", None)
+                dataset[variable_name] = dataset[variable_name].where(~failures, fill_value)  # type: ignore
         return dataset
 
 
