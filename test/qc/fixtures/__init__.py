5b2f72dfcb8cbe02e8dbe5f78263183d0420b2f3
Jason M. Cherry <jcherry@gmail.com>
2024-03-29 09:41:20 -0700

    Refactored QC tests, split tests & fixtures into seperate files.

---
+++ test/qc/fixtures/__init__.py
@@ -0,0 +1,2 @@
+from .sample_dataset import sample_dataset
+from .sample_dataset_2d import sample_dataset_2d
---
+++ test/qc/test_monotonic_with_2d_vars.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Any
+import numpy as np
+import xarray as xr
+
+from tsdat.qc.checkers import CheckMonotonic
+
+
+def test_monotonic_with_2d_vars(sample_dataset_2d: xr.Dataset, caplog: Any):
+    with caplog.at_level(logging.WARNING):
+        failures = CheckMonotonic().run(sample_dataset_2d, "wind_speed")
+    assert failures is None
+    assert (
+            "Variable 'wind_speed' has shape '(3, 4)'. 2D variables must provide a 'dim'"
+            " parameter"
+            in caplog.text
+    )
+
+    # Regular check
+    checker = CheckMonotonic(parameters=CheckMonotonic.Parameters(dim="time"))
+    failures = checker.run(sample_dataset_2d, "wind_speed")
+    assert not failures.any()
+
+    # # Manipulate the data
+    ds = sample_dataset_2d.copy(deep=True)
+    ds["wind_speed"].data += np.array([[4, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    checker = CheckMonotonic(parameters=CheckMonotonic.Parameters(dim="time"))
+    failures = checker.run(ds, "wind_speed")
+    expected = np.array(
+        [
+            [True, False, False, False],
+            [False, False, False, False],
+            [False, False, False, False],
+        ]
+    )
+    assert (failures == expected).all()
