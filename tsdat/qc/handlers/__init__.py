34e2dbb6fd9bedfd35b7fb34b8a5059ba3c6e125
Jason M. Cherry <jcherry@gmail.com>
2024-05-16 07:31:42 -0700

    Added unit test for CubicSplineInterp.

--- tsdat/qc/handlers/__init__.py
+++ tsdat/qc/handlers/__init__.py
@@ -1,3 +1,4 @@
+from .cubic_spline_interp import CubicSplineInterp
 from .data_quality_error import DataQualityError
 from .fail_pipeline import FailPipeline
 from .record_quality_results import RecordQualityResults
@@ -5,6 +6,7 @@ from .remove_failed_values import RemoveFailedValues
 from .sort_dataset_by_coordinate import SortDatasetByCoordinate
 
 __all__ = [
+    "CubicSplineInterp",
     "DataQualityError",
     "FailPipeline",
     "RecordQualityResults",
