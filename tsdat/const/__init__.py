900caa0d9e7fbe1aec2980df002223c1dad41acb
Jason M. Cherry <jcherry@gmail.com>
2024-06-17 09:26:22 -0700

    Added new const, from utils.

--- tsdat/const/__init__.py
+++ tsdat/const/__init__.py
@@ -1,6 +1,1 @@
-from .const import COORDINATE_SYSTEM
-from .const import INPUT_DATASTREAM
-from .const import OUTPUT_DATASTREAM
-
-from .const import InputKey
-from .const import VarName
+from .const import *
