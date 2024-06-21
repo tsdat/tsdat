8f2fcde6d84009d46a08d209c79e705414ec4050
Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>
2024-06-20 17:44:11 -0700

    Replace * import

--- tsdat/const/__init__.py
+++ tsdat/const/__init__.py
@@ -1,1 +1,7 @@
-from .const import *
+from .const import COORDINATE_SYSTEM as COORDINATE_SYSTEM
+from .const import DATASTREAM_TEMPLATE as DATASTREAM_TEMPLATE
+from .const import FILENAME_TEMPLATE as FILENAME_TEMPLATE
+from .const import INPUT_DATASTREAM as INPUT_DATASTREAM
+from .const import OUTPUT_DATASTREAM as OUTPUT_DATASTREAM
+from .const import InputKey as InputKey
+from .const import VarName as VarName
