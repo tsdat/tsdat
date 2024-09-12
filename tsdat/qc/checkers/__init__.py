c1680d0eb05ea1e4cefc1e29fd3cc164c3889ed0
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2024-09-12 09:40:27 -0700

    Create standard deviation test

--- tsdat/qc/checkers/__init__.py
+++ tsdat/qc/checkers/__init__.py
@@ -1,22 +1,28 @@
 from .check_array_mask_threshold import CheckArrayMaskThreshold
+
 from .check_fail_delta import CheckFailDelta
 from .check_fail_max import CheckFailMax
 from .check_fail_min import CheckFailMin
 from .check_fail_range_max import CheckFailRangeMax
 from .check_fail_range_min import CheckFailRangeMin
+from .check_fail_std import CheckFailStd
+
 from .check_missing import CheckMissing
 from .check_monotonic import CheckMonotonic
-from .check_outliers import CheckOutliers
+
 from .check_valid_delta import CheckValidDelta
 from .check_valid_max import CheckValidMax
 from .check_valid_min import CheckValidMin
 from .check_valid_range_max import CheckValidRangeMax
 from .check_valid_range_min import CheckValidRangeMin
+from .check_valid_std import CheckValidStd
+
 from .check_warn_delta import CheckWarnDelta
 from .check_warn_max import CheckWarnMax
 from .check_warn_min import CheckWarnMin
 from .check_warn_range_max import CheckWarnRangeMax
 from .check_warn_range_min import CheckWarnRangeMin
+from .check_warn_std import CheckWarnStd
 
 __all__ = [
     "CheckArrayMaskThreshold",
@@ -25,17 +31,19 @@ __all__ = [
     "CheckFailMin",
     "CheckFailRangeMax",
     "CheckFailRangeMin",
+    "CheckFailStd",
     "CheckMissing",
     "CheckMonotonic",
-    "CheckOutliers",
     "CheckValidDelta",
     "CheckValidMax",
     "CheckValidMin",
     "CheckValidRangeMax",
     "CheckValidRangeMin",
+    "CheckValidStd",
     "CheckWarnDelta",
     "CheckWarnMin",
     "CheckWarnMax",
     "CheckWarnRangeMax",
     "CheckWarnRangeMin",
+    "CheckWarnStd",
 ]
---
+++ tsdat/qc/checkers/check_std.py
@@ -0,0 +1,40 @@
+from abc import ABC
+from typing import Union
+
+import numpy as np
+import xarray as xr
+from numpy.typing import NDArray
+
+from .threshold_checker import ThresholdChecker
+
+
+class CheckStd(ThresholdChecker, ABC):
+    """Checks data for elements greater than a specified number of standard deviations
+    away from the mean.
+
+    The value of the threshold is specified by an attribute on each data variable, and
+    the attribute to search for is specified as a property of this base class.
+
+    If the specified attribute does not exist on the variable being checked then no
+    failures will be reported."""
+
+    def run(
+        self,
+        dataset: xr.Dataset,
+        variable_name: str,
+    ) -> Union[NDArray[np.bool_], None]:
+
+        var_data = dataset[variable_name].where(
+            dataset[variable_name] != dataset[variable_name]._FillValue
+        )
+        failures: NDArray[np.bool_] = np.zeros_like(var_data, dtype=np.bool_)  # type: ignore
+
+        threshold = self._get_threshold(dataset, variable_name, True)
+        if threshold is None:
+            return None
+
+        std_dev = var_data.std(dim="time", ddof=1)
+        mean = var_data.mean(dim="time")
+        failures = (var_data > mean + std_dev * threshold).values
+
+        return failures
