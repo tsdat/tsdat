0935c745bda7c7cea710aca75ae68fe9d7afb2a0
Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>
2024-11-05 13:19:06 -0800

    Define kVAR units (#235)
    * Define kVAR units
    
    * 'var' is the SI unit, 'VAR' is common in industry

--- tsdat/config/variables/ureg.py
+++ tsdat/config/variables/ureg.py
@@ -11,19 +11,28 @@ ureg.define("@alias degree = degN")
 ureg.define("@alias degree = degree_east")
 ureg.define("@alias degree = degree_E")
 ureg.define("@alias degree = degE")
+
+# Power
+ureg.define("var = volt * ampere * imaginary")
+ureg.define("@alias var = VAR")
+
 # Temperature
 ureg.define("@alias degree_Fahrenheit = degree_F")
 ureg.define("@alias degree_Celsius = degree_C")
 ureg.define("@alias degree_Rankine = degree_R")
 ureg.define("@alias kelvin = Kelvin")
+
 # Salinity
 ureg.define("psu = []")
+
 # Parts per
 ureg.define("ppm = 1e-6")
 ureg.define("ppb = 1e-9")
 ureg.define("ppt = 1e-12")
+
 # Percent
 ureg.define("@alias percent = %")
+
 # Other
 ureg.define("fraction = []")
 ureg.define("unitless = []")
